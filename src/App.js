8a9559337739553b4a409254993a3904dbc85a38
grandpaaa <bekzat.sul@mail.ru>
2021-03-09 00:51:45 +0600

    new files

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Router>
           <Switch>
             <Route exact path="/dev_bekzat" component={Main}/>
-            <Route path="/about" component={About}/>
+            <Route exact path="/about" component={About}/>
           </Switch>
         </Router>
     </div>
